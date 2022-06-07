fbd86e35368798469d75aa90052b11c3c2169bca
ashish <95022836+ashnew1226@users.noreply.github.com>
2022-06-07 12:18:01 +0530

    mainjs

--- Portfolio/script/main.js
+++ Portfolio/script/main.js
@@ -1,11 +1,11 @@
 
 
-// const hamburger = document.querySelector('.hamburger');
-// const navLink = document.querySelector('.nav__link');
+const hamburger = document.querySelector('.hamburger');
+const navLink = document.querySelector('.nav__link');
 
-// hamburger.addEventListener('click', () => {
-//   navLink.classList.toggle('hide');
-// });
+hamburger.addEventListener('click', () => {
+  navLink.classList.toggle('hide');
+});
 
 //Tournaments page slide show
 
